45cc295188596f1c6f1d5ed33a0d8389f7a7d8aa
Simun <127319486+SimunOnio@users.noreply.github.com>
2023-06-14 02:37:12 +0200

    Update predictRoughness.py

--- predictRoughness.py
+++ predictRoughness.py
@@ -7,20 +7,25 @@ from cropImage import cropImage
 
 print("[INFO] Initialising surface roughness prediction model...")
 # Set the image size and channels
-img_size = 500
+img_size = 1024
 # Set the image prediction label
 label_map = {0: "N5", 1: "N6", 2: "N7"}
 
 # Load the test image
 print("[INFO] Loading test image...")
-img = cv2.imread("samples/Sample7A.jpg", cv2.IMREAD_GRAYSCALE)
+img = cv2.imread("samples/Sample20A.jpg", cv2.IMREAD_GRAYSCALE)
 print("[INFO] Image was loaded successfully...")
 
 # Image augmentation & preprocessing
 print("[INFO] Processing image...")
-img = cropImage(img, img_size)
 img = preprocessImage(img)
+# img = cropImage(img, img_size)
+cropped_size = img_size // 3
+first = cropped_size
+mid = 2 * cropped_size
+img = img[first:mid, first:mid]
 print("[INFO] Image was processed successfully...")
+
 
 # Add an extra dimension to match the shape of the training data
 img = np.expand_dims(img, axis=0)
@@ -29,7 +34,7 @@ img = np.expand_dims(img, axis=-1)
 
 # Load the saved model
 print("[INFO] Loading surface roughness prediction model...")
-model = load_model("roughness_classifier/roughness_classifier3.h5")
+model = load_model("roughness_classifier/roughness_classifier5.h5")
 print("[INFO] The surface roughness prediction model was loaded successfully...")
 
 print("[INFO] Predicting surface roughness...")
